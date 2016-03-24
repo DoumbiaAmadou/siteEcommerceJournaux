60a13a2310e8bbdd3ffe9ffee68e5cd98eb497f8
Doumbia Amadou <doumbia.amado@gmail.com>
2016-03-24 02:26:09 +0100

    upload image done

--- src/journalBundle/Entity/Media.php
+++ src/journalBundle/Entity/Media.php
@@ -3,12 +3,14 @@
 namespace journalBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints  as Assert ; 
 
 /**
  * Media
  *
  * @ORM\Table(name="media")
  * @ORM\Entity(repositoryClass="journalBundle\Repository\MediaRepository")
+ * @ORM\HasLifecycleCallbacks
  */
 class Media
 {
@@ -21,21 +23,93 @@ class Media
      */
     private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="path", type="text")
-     */
-    private $path;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="alt", type="text")
+     * @ORM\Column(name="name",type="string",length=255)
+     * @Assert\NotBlank
      */
-    private $alt;
+    public $name ;
+    
+    
+    /**
+     * @ORM\Column(type="text", length=255, nullable=true)
+     */
+    public $path ; 
 
-
+    
+    public $file;
+    
+    
+    public function getUploadRootDir()
+    {
+        return __DIR__.'../../../../web/uploads' ; 
+    }
+    public function getAbsolutePath(){
+        return  null === $this->path 
+                ? null 
+                : $this->getUploadRootDir().'/'.$this->path ; 
+    }
+   
+    /**
+     * @var \DateTime 
+     * @ORM\Column(name="updateTime" ,type="datetime" , nullable= true )
+     */
+    private  $updateTime ; 
+    
+    /**
+     * @ORM\PostLoad()
+     */
+    public function postLoad(){
+        $this->updateTime = new \DateTime() ; 
+    }
+        /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload(){
+         if(file_exists($this->getUploadRootDir())){
+             $this->tmpFile = $this->getAbsolutePath() ; 
+             $this->oldFile = $this->getPath() ; 
+             $this->updateTime = new \DateTime();
+         }
+         else
+         {
+                $this->tmpFile = null ; 
+                $this->oldFile = null ; 
+         }
+        if(null !== $this->file){
+            $this->path = sha1(uniqid(mt_rand(), true)).'.'.$this->file->guessExtension();
+        }
+    }
+       /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload(){
+        
+       if(null !== $this->file){
+           $this->file->move($this->getUploadRootDir(),$this->path); 
+           unset($this->file); 
+           
+           if($this->oldFile!=null) unlink ($this->tmpFile);
+               
+       }
+    }
+    
+    /**
+     * @ORM\PreRemove()
+     * 
+     */
+    public function preRemoveUpload(){
+        $this->tmpFile = $this->getAbsolutePath(); 
+        
+    }
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload(){
+        if(file_exists($this->tmpFile)) unlink ($this->tmpFile); 
+    }
     /**
      * Get id
      *
@@ -52,11 +126,9 @@ class Media
      * @param string $path
      * @return Media
      */
-    public function setPath($path)
+    public function getName()
     {
-        $this->path = $path;
-
-        return $this;
+       return $this->name;
     }
 
     /**
@@ -69,26 +141,8 @@ class Media
         return $this->path;
     }
 
-    /**
-     * Set alt
-     *
-     * @param string $alt
-     * @return Media
-     */
-    public function setAlt($alt)
-    {
-        $this->alt = $alt;
-
-        return $this;
-    }
-
-    /**
-     * Get alt
-     *
-     * @return string 
-     */
-    public function getAlt()
-    {
-        return $this->alt;
-    }
+   public function gertAssertPath(){
+       return 'uploads/'.$this->path ; 
+   } 
+    
 }
